faca5c049585c7de165e8319f836c044eeaec72c
hgarud <hrishi.garud@gmail.com>
2024-01-04 20:32:14 -0800

    add nougat inference logic and save extracted text to a txt file

--- main.py
+++ main.py
@@ -3,6 +3,8 @@
 import argparse
 import logging
 import os
+from PIL import Image
+import re
 import requests
 import tempfile
 from typing import List, Union
@@ -45,7 +47,39 @@ def main(args):
     for paper_path in paper_paths:
         # rasterize the pdf into images
         paper_pages = rasterize_paper(paper_path)
-
+        predictions = []
+        for i, paper_page in enumerate(paper_pages):
+            page_image = Image.open(paper_page)
+            page_image = model.encoder.prepare_input(page_image, random_padding=False)
+            model_output = model.inference(image_tensors=page_image.unsqueeze(dim=0))
+            # check if model output is faulty
+            output = model_output["predictions"][0]
+            if i == 0:
+                logging.info(
+                    "Processing file %s with %i pages"
+                    % (paper_path.name, i)
+                )
+            if output.strip() == "[MISSING_PAGE_POST]":
+                # uncaught repetitions -- most likely empty page
+                predictions.append(f"\n\n[MISSING_PAGE_EMPTY:{i}]\n\n")
+            elif model_output["repeats"][0] is not None:
+                if model_output["repeats"][0] > 0:
+                    # If we end up here, it means the output is most likely not complete and was truncated.
+                    logging.warning(f"Skipping page {i} due to repetitions.")
+                    predictions.append(f"\n\n[MISSING_PAGE_FAIL:{i}]\n\n")
+                else:
+                    # If we end up here, it means the document page is too different from the training domain.
+                    # This can happen e.g. for cover pages.
+                    predictions.append(
+                        f"\n\n[MISSING_PAGE_EMPTY:{i}]\n\n"
+                    )
+            else:
+                predictions.append(output)
+        out_text = "".join(predictions).strip()
+        out_text = re.sub(r"\n{3,}", "\n\n", out_text).strip()
+        out_path = args.output_dir / "txts" / (paper_path.stem + ".txt")
+        out_path.parent.mkdir(parents=True, exist_ok=True)
+        out_path.write_text(out_text)
 
 
 if __name__ == '__main__':
